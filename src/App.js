739a3e84a64eed6e369e2dc3ab1abc0856b56e0a
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-07 18:42:28 +0530

    add className=box1-1 and className=box1-2

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { useEffect } from "react";
 import gsap from "gsap";
 
 function App() {
+  useEffect(() => {
+    const t1 = gsap.timeline();
+  }, []);
+
   return (
     <div className="app">
-      <div className="box_1">
+      <div className="box1">
         <h1>
-          Hi Everyone, <span> I am Shailesh Bisht</span>
+          <span className="box1-1">Hi Everyone ,</span>
+          <span className="box1-2"> I am Shailesh Bisht</span>
         </h1>
       </div>
     </div>
