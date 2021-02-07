a62a28fd072844337916135954ff5f215c8010bf
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-07 19:09:17 +0530

    add .box2 and span

--- src/App.js
+++ src/App.js
@@ -15,12 +15,15 @@ function App() {
         duration: 1.5,
         background: "blue",
         ease: "steps(12)",
+        borderRadius: "50px",
+        padding: "10px",
       })
       .to(
         ".box1-1",
         { duration: 1.5, color: "white", ease: "steps(12)" },
         "-=1.5"
-      );
+      )
+      .to(".box1-2", { duration: 2, opacity: 1, textDecoration: "underline" });
   }, []);
 
   return (
@@ -30,6 +33,13 @@ function App() {
           <span className="box1-1">Hi Everyone ,</span>
           <span className="box1-2"> I am Shailesh Bisht</span>
         </h1>
+      </div>
+      <div className="box2">
+        <span>This </span>
+        <span>is </span>
+        <span>a </span>
+        <span>random </span>
+        <span>animation</span>
       </div>
     </div>
   );
