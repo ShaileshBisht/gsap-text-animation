dcdddcb43f4561b085e93522e6df67558e76446a
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-07 18:32:24 +0530

    add style to .app div

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ function App() {
   return (
     <div className="app">
       <div className="box_1">
-        <h1>Hi Everyone, I am Shailesh Bisht</h1>
+        <h1>
+          Hi Everyone, <span> I am Shailesh Bisht</span>
+        </h1>
       </div>
     </div>
   );
