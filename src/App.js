7756599ade365a5f7bd2980c473465eff022fe22
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-07 19:22:12 +0530

    add style to  .box2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import gsap from "gsap";
 function App() {
   useEffect(() => {
     const t1 = gsap.timeline();
+    const t2 = gsap.timeline();
     t1.from(".box1", {
       duration: 1,
       opacity: 0,
@@ -18,12 +19,10 @@ function App() {
         borderRadius: "50px",
         padding: "10px",
       })
-      .to(
-        ".box1-1",
-        { duration: 1.5, color: "white", ease: "steps(12)" },
-        "-=1.5"
-      )
+      .to(".box1-1", { duration: 1.5, color: "white", ease: "steps(12)" })
       .to(".box1-2", { duration: 2, opacity: 1, textDecoration: "underline" });
+
+    t2.from(".box2-1", { duration: 1, scale: 3, opacity: 0, stagger: 1 });
   }, []);
 
   return (
@@ -35,11 +34,11 @@ function App() {
         </h1>
       </div>
       <div className="box2">
-        <span>This </span>
-        <span>is </span>
-        <span>a </span>
-        <span>random </span>
-        <span>animation</span>
+        <div className="box2-1">This </div>
+        <div className="box2-1">is </div>
+        <div className="box2-1">a </div>
+        <div className="box2-1">random </div>
+        <div className="box2-1">animation</div>
       </div>
     </div>
   );
